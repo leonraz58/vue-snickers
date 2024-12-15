ef10f8ff35a605083855e57f201e57f33d503fd1
leonraz58 <leonraz58@gmail.com>
2024-12-16 01:41:02 +0300

    add CartItem

---
+++ src/components/CartItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex items-center border border-slate-200 p-4 rounded-xl gap-4">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="">
+
+    <div class="flex flex-col">
+      <p>Кроссовки Nike 11111111111111</p>
+
+      <div class="flex justify-between mt-2">
+        <b>1234 руб.</b>
+        <img class="opacity-40 hover:opacity-100 cursor-pointer transition" src="/close.svg" alt="закрыть">
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import DrawerHead from '@/components/DrawerHead.vue'
+import CartItem from '@/components/CartItem.vue'
 </script>
 
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead/>
+    <CartItem/>
+    <CartItem/>
+    <CartItem/>
+    <CartItem/>
   </div>
 </template>
