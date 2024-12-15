4cb161c8c612a26b9b6a797b910b812a32bdbae3
leonraz58 <leonraz58@gmail.com>
2024-12-15 03:40:46 +0300

    delete example code

---
+++ src/components/MyHello.vue
@@ -0,0 +1,14 @@
+<script setup>
+alert('hello world!')
+</script>
+
+<template>
+  <p class="text">это мой компонент</p>
+  <button>открыть</button>
+</template>
+
+<style scoped>
+.text {
+  background: red;
+}
+</style>
