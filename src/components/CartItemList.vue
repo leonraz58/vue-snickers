9c2c2f8b2871bcb5645f4abf44ec912941f5f6f6
leonraz58 <leonraz58@gmail.com>
2024-12-16 01:47:05 +0300

    add CartItemList

---
+++ src/components/CartItemList.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import CartItem from '@/components/CartItem.vue'
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <CartItem/>
+    <CartItem/>
+    <CartItem/>
+    <CartItem/>
+  </div>
+</template>
--- src/components/DrawerHead.vue
+++ src/components/DrawerHead.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center gap-5 mb-5">
+  <div class="flex items-center gap-5 mb-8">
     <svg
       class="rotate-180 hover:-translate-x-1 opacity-30 hover:opacity-100 transition cursor-pointer"
       width="16"
