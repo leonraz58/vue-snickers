a654a3003f21d698757604530a0076f89d241963
leonraz58 <leonraz58@gmail.com>
2024-12-15 20:40:09 +0300

    configure tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,11 @@ import MyHello from '@/components/MyHello.vue'
 </script>
 
 <template>
+  <div class="mb-20 bg-amber-100">
   <MyHello title="Кроссовки Puma" :price="9500" :isAdded="true">
     <button>Добавить</button>
   </MyHello>
-  <MyHello isAdded />
-  <MyHello>
-    <MyHello>
-      <button>123</button>
-    </MyHello>
-  </MyHello>
+  </div>
 </template>
 
 <style scoped>
