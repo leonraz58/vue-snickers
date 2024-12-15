fd46b294030f851b9b1bc07bb9c5ce8515839ebe
leonraz58 <leonraz58@gmail.com>
2024-12-15 04:20:55 +0300

    test composition api

--- src/components/MyHello.vue
+++ src/components/MyHello.vue
@@ -1,19 +1,41 @@
-<script>
-export default {
-  data() {
-    return {
-      count: 0,
-    }
-  },
-  methods: {
-    increment() {
-      this.count++
-    }
-  }
+<script setup>
+import { ref } from 'vue'
+
+let count = ref(0)
+
+function increment() {
+  count.value++
 }
+
+// function updateCount(event) {
+//   count.value = Number(event.target.value)
+// }
 </script>
 
 <template>
   <h1>{{ count }}</h1>
   <button @click="increment">+</button>
+<!--  <input @input="updateCount" />-->
+  <input v-model="count" />
+  <input @keyup.enter="increment" />
 </template>
+
+<!--<script>-->
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      count: 0,-->
+<!--    }-->
+<!--  },-->
+<!--  methods: {-->
+<!--    increment() {-->
+<!--      this.count++-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <h1>{{ count }}</h1>-->
+<!--  <button @click="increment">+</button>-->
+<!--</template>-->
