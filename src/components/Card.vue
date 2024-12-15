33cf666cec177de2c776473f10707f1d76b5c98d
leonraz58 <leonraz58@gmail.com>
2024-12-16 00:28:08 +0300

    add Card props

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,17 +1,37 @@
+<script setup>
+defineProps({
+  imageUrl: String,
+  title: String,
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onClickAdd: Function,
+  onClickFavorite: Function
+})
+
+</script>
+
 <template>
   <div>
-    <div class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
-      <img src="/like-2.svg" alt="Like 1" class="absolute top-8 left-8"/>
-      <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
-      <p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+    <div
+      class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
+    >
+      <img
+        @click="onClickFavorite"
+        :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+        alt="Like 1"
+        class="absolute top-8 left-8"
+      />
+      <img :src="imageUrl" alt="Sneaker" />
+      <p class="mt-2">{{ title }}</p>
 
       <div class="flex justify-between">
         <div class="flex flex-col">
           <span class="text-slate-400">Цена:</span>
-          <b>1205 р.</b>
+          <b>{{ price }} р.</b>
         </div>
 
-        <img src="/plus.svg" alt="Plus">
+        <img @click="onClickAdd" :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
       </div>
     </div>
   </div>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,16 +1,25 @@
 <script setup>
 import Card from '@/components/Card.vue'
+
+const onClickAdd = () => {
+  alert(1)
+}
+
+const onClickFavourite = () => {
+  alert(2)
+}
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card
+      imageUrl="/sneakers/sneakers-1.jpg"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="1205"
+      :is-added="true"
+      :is-favorite="true"
+      :onClickAdd="onClickAdd"
+      :on-click-favorite="onClickFavourite"
+    />
   </div>
 </template>
