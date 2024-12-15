352aa15961b5dd203971be64b6f29c4d98cd208e
leonraz58 <leonraz58@gmail.com>
2024-12-16 02:02:21 +0300

    add Drawer footer

--- src/components/CartItemList.vue
+++ src/components/CartItemList.vue
@@ -3,7 +3,7 @@ import CartItem from '@/components/CartItem.vue'
 </script>
 
 <template>
-  <div class="flex flex-col gap-4">
+  <div class="flex flex-col flex-1 justify-between">
     <CartItem/>
     <CartItem/>
     <CartItem/>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -8,5 +8,25 @@ import CartItemList from '@/components/CartItemList.vue'
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead/>
     <CartItemList/>
+
+    <div class="flex flex-col gap-4 mt-7">
+      <div class="flex gap-2">
+        <span>Итого:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>12990 руб.</b>
+      </div>
+
+      <div class="flex gap-2">
+        <span>Налог 5%</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>900 руб.</b>
+      </div>
+
+      <button class="mt-4 bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
+              :disabled="false"
+      >
+        Оформить заказ
+      </button>
+    </div>
   </div>
 </template>
