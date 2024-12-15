d0689f828710a91a2a8ca7aaeb428d381ee9cbaf
leonraz58 <leonraz58@gmail.com>
2024-12-15 23:20:31 +0300

    add Card

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import Header from '@/components/Header.vue'
+import Card from '@/components/Card.vue'
 </script>
 
 <template>
-  <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14">
-    <Header/>
+  <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14">
+    <Header />
 
-    <h1>Text</h1>
+    <Card/>
+    <Card/>
+    <Card/>
   </div>
 </template>
 
