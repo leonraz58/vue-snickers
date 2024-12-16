23f5c72e708ccf8b8eb5e56b04227aa149f467b9
leonraz58 <leonraz58@gmail.com>
2024-12-16 15:54:31 +0300

    v-for Card

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,6 +1,10 @@
 <script setup>
 import Card from '@/components/Card.vue'
 
+defineProps({
+  items: Array
+})
+
 const onClickAdd = () => {
   alert(1)
 }
@@ -13,9 +17,11 @@ const onClickFavourite = () => {
 <template>
   <div class="grid grid-cols-4 gap-5">
     <Card
-      imageUrl="/sneakers/sneakers-1.jpg"
-      title="Мужские Кроссовки Nike Blazer Mid Suede"
-      :price="1205"
+      v-for="item in items"
+      :key="item.id"
+      :imageUrl="item.imageUrl"
+      :title="item.title"
+      :price="item.price"
       :is-added="true"
       :is-favorite="true"
       :onClickAdd="onClickAdd"
