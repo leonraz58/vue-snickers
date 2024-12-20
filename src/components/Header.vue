8e157d74496721cafab460ee06e2f0b29fae1e9d
leonraz58 <leonraz58@gmail.com>
2024-12-20 22:39:05 +0300

    routing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,37 @@
 <script setup>
 defineProps({
-  totalPrice: Number
+  totalPrice: Number,
 })
 const emit = defineEmits(['openDriver'])
 </script>
 
 <template>
   <header class="flex justify-between border-b border-slate-200 px-10 py-8">
-    <div class="flex items-center gap-4">
-      <img src="/logo.png" alt="Logo" class="w-10" />
-      <div>
-        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
-        <p class="text-slate-400">Магазин лучших кроссовок</p>
+    <router-link to="/">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="Logo" class="w-10" />
+        <div>
+          <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+          <p class="text-slate-400">Магазин лучших кроссовок</p>
+        </div>
       </div>
-    </div>
+    </router-link>
 
     <ul class="flex items-center gap-10">
-      <li @click = "() => emit('openDriver')" class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+      <li
+        @click="() => emit('openDriver')"
+        class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black"
+      >
         <img src="/cart.svg" alt="Cart" />
         <b> {{ totalPrice }}</b>
       </li>
 
-      <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
-        <img src="/heart.svg" alt="Cart" />
-        <span>Закладки</span>
-      </li>
+      <router-link to="/favorites">
+        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+          <img src="/heart.svg" alt="Cart" />
+          <span>Закладки</span>
+        </li>
+      </router-link>
 
       <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
         <img src="/profile.svg" alt="Cart" />
--- src/main.js
+++ src/main.js
@@ -3,9 +3,23 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/pages/Home.vue'
+import Favorites from '@/pages/Favorites.vue'
 
 const app = createApp(App)
 
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/favorites', name: 'Favorites', component: Favorites },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+app.use(router)
 app.use(autoAnimatePlugin)
 
 app.mount('#app')
