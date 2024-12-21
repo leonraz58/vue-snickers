b119a0767decd2313129c9fbc23abc95f4706cfc
leonraz58 <leonraz58@gmail.com>
2024-12-21 03:18:44 +0300

    add order success form

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -5,15 +5,15 @@ import InfoBlock from '@/components/InfoBlock.vue'
 import axios from 'axios'
 import { computed, inject, ref } from 'vue'
 
-
 const props = defineProps({
   totalPrice: Number,
   vatPrice: Number,
 })
 
-const {cart, closeDrawer} = inject('cart')
+const { cart } = inject('cart')
 
 const isCreatingOrder = ref(false)
+const orderId = ref(null)
 
 const createOrder = async () => {
   try {
@@ -25,7 +25,7 @@ const createOrder = async () => {
 
     cart.value = []
 
-    return data
+    orderId.value = data.id
   } catch (err) {
     console.log(err)
   } finally {
@@ -41,10 +41,18 @@ const cartButtonDisabled = computed(() => isCreatingOrder.value || cartIsEmpty.v
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead />
 
-    <div v-if="!totalPrice" class="flex h-full items-center">
-      <InfoBlock title="Корзина пустая"
-                 description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ."
-                 image-url="/package-icon.png"
+    <div v-if="!totalPrice || orderId" class="flex h-full items-center">
+      <InfoBlock
+        v-if="!totalPrice && !orderId"
+        title="Корзина пустая"
+        description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ."
+        image-url="/package-icon.png"
+      />
+      <InfoBlock
+        v-if="orderId"
+        title="Заказ оформлен!"
+        :description="`Ваш заказ ${orderId} скоро будет передан курьерской доставке`"
+        image-url="/order-success-icon.png"
       />
     </div>
 
@@ -73,7 +81,5 @@ const cartButtonDisabled = computed(() => isCreatingOrder.value || cartIsEmpty.v
         </button>
       </div>
     </div>
-
-
   </div>
 </template>
